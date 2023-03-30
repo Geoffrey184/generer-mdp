df49b82673b2b4621b5d685287674e3224ddd435
Geoffrey184 <geoffreynono@icloud.com>
2023-03-30 11:26:22 +0200

    finish

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var el_down = document.getElementById("geeks");
-          
-        function mdp() {
-            el_down.innerHTML = 
-                Math.random().toString(36).slice(2) + 
-                Math.random().toString(36)
-                    .toUpperCase().slice(2);
-            } 
+
+function mdp(length) {
+  var symbols = "!@#$%^&*()_+-=[]{}|;':\"<>,.?/";
+  var password = "";
+  
+  while (password.length < length) {
+    password += Math.random().toString(36).slice(2);
+    password += symbols.charAt(Math.floor(Math.random() * symbols.length));
+    password += Math.random().toString(36).toUpperCase().slice(2);
+  }
+
+  password = password.slice(0, length); 
+  
+  el_down.innerHTML = password;
+
+
+ mdp(15); 
+
+}
+
+ 
+
+  
